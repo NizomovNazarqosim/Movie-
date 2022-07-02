125076579fa628170ce02b0ee00409c03d69240a
NizomovNazarqosim <nazarqosimnizomov@gmail.com>
2022-07-02 14:17:13 +0500

    movie

--- js/script.js
+++ js/script.js
@@ -1,15 +1,37 @@
+const elBody = document.querySelector("body");
 const elList = document.querySelector(".movie__list");
 const numResult = document.querySelector(".movie__result-num");
 const elFotmFilter = document.querySelector(".form-filter");
 const elSelect = document.querySelector(".select");
 const bookmarkedList = document.querySelector(".bookmarked-list");
+const nightBtn = document.querySelector(".night");
+const dayBtn = document.querySelector(".day");
+const bookmarkTitle = document.querySelector(".bookmark-title");
 
 numResult.textContent = films.length;
 
 // start of Bookmark
 
+// Start of Night && day mode
+
+nightBtn.addEventListener("click", function(e){
+
+   elBody.style.backgroundColor = "black";
+   bookmarkTitle.style.color = "white";
+});
+
+dayBtn.addEventListener("click", function(evt){
+  elBody.style.backgroundColor = "white";
+  bookmarkTitle.style = "black";
+})
+
+
+// End of Night && day mode
+
+
 const localStorageBookmark = JSON.parse(window.localStorage.getItem("bookmarks"))
 const bookmarks = localStorageBookmark || [];
+
 
 bookmarkedList.addEventListener("click", function(evt){
   if(evt.target.matches(".bookmark-delete-btn")){
@@ -39,7 +61,7 @@ const renderBookmars = function(arr, htmlElement){
     bookmarkItem.textContent = bookmark.title;
     bookmarkDeleteBtn.textContent = "Remove";
 
-window.localStorage.setItem("bookmarks", JSON.stringify(bookmarks))
+  window.localStorage.setItem("bookmarks", JSON.stringify(bookmarks));
 
     // setattribute
     bookmarkDeleteBtn.setAttribute("class", "bookmark-delete-btn btn btn-danger");
@@ -52,17 +74,18 @@ window.localStorage.setItem("bookmarks", JSON.stringify(bookmarks))
     bookmarkItem.appendChild(bookmarkDeleteBtn);
   })
 }
-
+renderBookmars(bookmarks, bookmarkedList);
 
 elList.addEventListener("click", function(evt){
 if(evt.target.matches(".btn-bookmark")){
   const bookmarkId = evt.target.dataset.bookmarkBtnId;
-
+  
   const foundBookmark = films.find(film => film.id === bookmarkId);
 
-if(!bookmarks.includes(foundBookmark)){
-  bookmarks.push(foundBookmark);
+const x = bookmarks.find(e => e.id == bookmarkId) 
 
+if(!x){
+  bookmarks.push(foundBookmark);
 }
 
 bookmarkedList.innerHTML = null;
@@ -82,6 +105,7 @@ const renderGenres = function(arr){
       film.genres.forEach(genre => {
        if(!uniqueGenres.includes(genre)){
         uniqueGenres.push(genre);
+        
        } 
     });
     });
@@ -102,18 +126,23 @@ const renderMovies = function(filmsArr, htmlElement){
      const newDiv = document.createElement("div");
      const newHeading = document.createElement("h5");
      const btnBookmark = document.createElement("button");
+     const overviewDiv = document.createElement("div");
  
      // Set attribute
-     newLi.setAttribute("class", "card mb-3");
+     newLi.setAttribute("class", "item-for card mb-3");
      newLi.style.width = "18rem";
+     newLi.style.height = "660px"
+     newLi.style.position = "relative"
      newImg.classList.add("card-img-top");
      newImg.setAttribute("src", movie.poster);
      newDiv.setAttribute("class", "card-body");
      newHeading.setAttribute("class", "card-title");
      btnBookmark.setAttribute("class", "btn-bookmark");
+     overviewDiv.setAttribute("class", "description")
 
     //  dataset
     btnBookmark.dataset.bookmarkBtnId = movie.id;
+    overviewDiv.textContent = movie.overview;
      
    
    
@@ -137,6 +166,7 @@ const renderMovies = function(filmsArr, htmlElement){
      newDiv.appendChild(newHeading);
      newDiv.appendChild(btnBookmark);
     newDiv.appendChild(genreList);
+    newDiv.appendChild(overviewDiv)
   
   }
 )}
